9a453f1fa18b0991e732b2c51123cbeee5c9ba4d
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-04-29 18:32:03 +0900

    Using lambda with map, filter, reduce

--- lambda_prac.py
+++ lambda_prac.py
@@ -1,6 +1,7 @@
+from audioop import reverse
 import os
 import sys
-
+from functools import reduce
 def hap(x, y):
     return x + y
 
@@ -11,5 +12,21 @@ def main():
     res = (lambda x, y: x + y)
     print(res(10, 20))
 
+    #Using lambda with map
+    a = [1, 2, 3, 4]
+    b = [10, 11, 12, 13]
+    res2 = list(map(lambda x, y: x+y, a,b))
+    print(res2)
+    res3 = list(map(lambda x,y: x*y, a,b))
+    print(res3)
+
+    #Using lambda with filter
+    foo = [2, 18, 9, 22, 17, 24, 8, 12, 27]
+    res4 = list(filter(lambda x: x%3 == 0, foo))
+    print(res4)
+
+    #Using lambda with reduce
+    res5 = reduce(lambda x,y: y + x, 'abcdefg')
+    print(res5.reverse())
 if __name__== '__main__':
     main()
