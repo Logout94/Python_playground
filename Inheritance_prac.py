3ef9f33d674ba76a17e86717e58b888c0044a079
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-08-04 00:13:26 +0900

    Adding method in child class with additional property

--- Inheritance_prac.py
+++ Inheritance_prac.py
@@ -15,13 +15,17 @@ class Student(Person):
         # super(): 부모 클래스의 모든 멤버 변수와 멤버 함수를 가져온다
         super().__init__(firstname, lastname)
         self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
 def main():
     p1 = Person("John", "Doe")
     p1.printname()
 
     p2 = Student("Mike", "Olsen", 2019)
     p2.printname()
-    print(p2.graduationyear)
-
+    p2.welcome()
+    
 if __name__=='__main__':
     main()
