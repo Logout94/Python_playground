670c56e8bcd67df6e0461669d58b15c29bd87c84
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-08-04 00:00:08 +0900

    Inherit person class and use the init function and methods

--- Inheritance_prac.py
+++ Inheritance_prac.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# Parent class
 class Person:
     def __init__(self, firstname, lastname):
         self.lastname = lastname
@@ -8,10 +9,16 @@ class Person:
     
     def printname(self):
         print(self.firstname, self.lastname)
+# Child class
+class Student(Person):
+    pass
 
 def main():
     p1 = Person("John", "Doe")
     p1.printname()
+
+    p2 = Student("Mike", "Olsen")
+    p2.printname()
 
 if __name__=='__main__':
     main()
