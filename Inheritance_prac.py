2d5ee220b740c7db75a55af99009273c8858df34
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-08-04 00:09:23 +0900

    Using super() to get methods and properties from parent class

--- Inheritance_prac.py
+++ Inheritance_prac.py
@@ -11,14 +11,17 @@ class Person:
         print(self.firstname, self.lastname)
 # Child class
 class Student(Person):
-    pass
-
+    def __init__(self, firstname, lastname):
+        # super(): 부모 클래스의 모든 멤버 변수와 멤버 함수를 가져온다
+        super().__init__(firstname, lastname)
+        self.graduationyear = 2019
 def main():
     p1 = Person("John", "Doe")
     p1.printname()
 
     p2 = Student("Mike", "Olsen")
     p2.printname()
+    print(p2.graduationyear)
 
 if __name__=='__main__':
     main()
